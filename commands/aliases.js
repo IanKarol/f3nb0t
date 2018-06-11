4f1ed32dc8ce48cdf44d24355ce3c04178ef8798
IanKarol <37708687+IanKarol@users.noreply.github.com>
2018-06-11 23:01:50 +0200

    Updated colors

--- commands/aliases.js
+++ commands/aliases.js
@@ -1,10 +1,11 @@
 const Discord = require("discord.js");
+const config = require("../botconfig.json");
 
 exports.run = async (bot, message, args) => {
 
     let aliasEmbed = new Discord.RichEmbed()
     .setAuthor("f3nb0t", bot.user.displayAvatarURL)
-    .setColor("#FF0000")
+    .setColor(config.green)
     .addField("Aliases", "f!clear > clr\nf!codeblock > cb / code\nf!help > h / halp\nf!info > i\nf!ping > latency / myping\nf!plug > p / stream\nf!statusclear > sc")
     .setFooter("Not case sensitive");
 
