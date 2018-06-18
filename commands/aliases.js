6a891291ece4a80a3906688b2272d3d92d5cf143
IanKarol <37708687+IanKarol@users.noreply.github.com>
2018-06-18 21:37:04 +0200

    Added status alias

--- commands/aliases.js
+++ commands/aliases.js
@@ -6,7 +6,7 @@ exports.run = async (bot, message, args) => {
     let aliasEmbed = new Discord.RichEmbed()
     .setAuthor("f3nb0t", bot.user.displayAvatarURL)
     .setColor(config.green)
-    .addField("Aliases", "f!clear > clr\nf!codeblock > cb / code\nf!help > h / halp\nf!info > i\nf!ping > latency / myping\nf!plug > p / stream\nf!statusclear > sc")
+    .addField("Aliases", "f!clear > clr\nf!codeblock > cb / code\nf!help > h / halp\nf!info > i\nf!ping > latency / myping\nf!plug > p / stream\nf!status > s\nf!statusclear > sc")
     .setFooter("Case sensitive");
 
     message.channel.send(aliasEmbed);
